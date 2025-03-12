08c7906c002d94649a7464489950960f83e5b0e8
Roman Ambrosevich <etoxroman@gmail.com>
2025-03-12 14:41:50 +0300

    Fixed id class

--- src/main/java/com/example/vinyltrackerapi/api/models/UserVinylId.java
+++ src/main/java/com/example/vinyltrackerapi/api/models/UserVinylId.java
@@ -1,45 +1,14 @@
 package com.example.vinyltrackerapi.api.models;
 
 import java.io.Serializable;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserVinylId implements Serializable {
     private Integer user;
     private Integer vinyl;
-
-    public UserVinylId() {}
-
-    public UserVinylId(Integer user, Integer vinyl) {
-        this.user = user;
-        this.vinyl = vinyl;
-    }
-
-    public Integer getUser() {
-        return user;
-    }
-
-    public void setUser(Integer user) {
-        this.user = user;
-    }
-
-    public Integer getVinyl() {
-        return vinyl;
-    }
-
-    public void setVinyl(Integer vinyl) {
-        this.vinyl = vinyl;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof UserVinylId that)) return false; // Новый стиль
-        return Objects.equals(user, that.user) &&
-                Objects.equals(vinyl, that.vinyl);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(user, vinyl);
-    }
 }
